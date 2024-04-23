a21f993422a7892786ad88ea8a654e151e83a998
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-04-23 16:01:17 +0300

    add navigate

--- BookElves/src/App.tsx
+++ BookElves/src/App.tsx
@@ -4,21 +4,47 @@ import Home from './views/Home/Home';
 import About from './views/About/About';
 import Login from './views/Login/Login';
 import Register from './views/Register/Register';
+import Header from './components/Header/Header';
+import { AppContext } from './context/appContext';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './config/firebase-config';
+import { getUserData } from './service/user';
 
 function App() {
+  const [context, setContext] = useState({
+    user: {} as any,
+    userData: {} as any,
+  });
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      getUserData(user.uid)
+        .then(snapshot => {
+          if (snapshot.exists()) {
+            setContext({ user, userData: snapshot.val()[Object.keys(snapshot.val())[0]] });
+          }
+        });
+    }
+  }, [user]);
 
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
-
-        </Routes>
+        <AppContext.Provider value={{ ...context, setContext }}>
+          <Header />
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="*" element={<h1>Not Found</h1>} />
+            </Routes>
+          </div>
+        </AppContext.Provider>
       </BrowserRouter>
     </>
   );
---
+++ BookElves/src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAppContext } from "../../context/appContext";
+import { logoutUser } from "../../service/auth";
+
+const Header = () => {
+    const navigate = useNavigate();
+    const { user, userData, setContext } = useAppContext();
+    
+    const logout = async () => {
+        await logoutUser();
+        setContext({ user: null, userData: null });
+        navigate('/home');
+    }
+
+
+
+    return (
+        <>
+            <NavLink to="/home" className='home'>Home </NavLink>
+            <NavLink to="/about" className='about'>About</NavLink>
+            {!user && <NavLink to="/register" className='register'>Register</NavLink>}
+            {!user && <NavLink to="/login" className='login'>Login</NavLink>}
+            {user && <button onClick={logout} className='logout'>Logout</button>}
+            {user && <NavLink to="/profile" className='profile'>Profile</NavLink>}
+            
+        </>
+    )
+}
+
+export default Header;
