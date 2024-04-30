a752f28d7df2a0fb0981758bd521cf4ff6b9fd3d
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-04-30 21:51:50 +0300

    add header

--- BookElves/src/components/Header/Header.tsx
+++ BookElves/src/components/Header/Header.tsx
@@ -1,31 +1,41 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAppContext } from "../../context/appContext";
 import { logoutUser } from "../../service/auth";
+import houseDoor from 'bootstrap-icons/icons/house-door.svg';
+import balloonHeart from 'bootstrap-icons/icons/balloon-heart.svg';
+import peopleFill from 'bootstrap-icons/icons/people-fill.svg';
+import plusCircle from 'bootstrap-icons/icons/plus-circle.svg';
+
+
+
+
+import './Header.css';
 
 const Header = () => {
-    const navigate = useNavigate();
-    const { user, userData, setContext } = useAppContext();
-    
-    const logout = async () => {
-        await logoutUser();
-        setContext({ user: null, userData: null });
-        navigate('/home');
-    }
+  const navigate = useNavigate();
+  const { user, userData, setContext } = useAppContext();
+
+  const logout = async () => {
+    await logoutUser();
+    setContext({ user: null, userData: null });
+    navigate('/home');
+  }
 
 
 
-    return (
-        <>
-            <NavLink to="/home" className='home'>Home </NavLink>
-            <NavLink to="/about" className='about'>About</NavLink>
-            {!user && <NavLink to="/register" className='register'>Register</NavLink>}
-            {!user && <NavLink to="/login" className='login'>Login</NavLink>}
-            {user && <button onClick={logout} className='logout'>Logout</button>}
-            {user && <NavLink to="/profile" className='profile'>Profile</NavLink>}
-            {user && <NavLink to="/create" className='create-post'>Create</NavLink>}
-            
-        </>
-    )
+  return (
+    <div className="nav-links">
+      {user && <button onClick={logout} className='logout'>Logout</button>}
+      <NavLink to="/home" className='home'><img src={houseDoor} alt="Home" /></NavLink>
+      {user && <NavLink to="/Like" className='create-post'><img src={balloonHeart} alt="Balloon Heart" /></NavLink>}
+      {user && <NavLink to="/create" className='create-post'><img src={plusCircle} alt="Plus Circle" /></NavLink>}
+      {user && <NavLink to="/friends" className='friends'><img src={peopleFill} alt="Friends" /></NavLink>}
+      <NavLink to="/about" className='about'>About</NavLink>
+      {!user && <NavLink to="/register" className='register'>Register</NavLink>}
+      {!user && <NavLink to="/login" className='login'>Login</NavLink>}
+      {user && <NavLink to="/profile" className='profile'>Profile</NavLink>}
+    </div>
+  )
 }
 
 export default Header;
