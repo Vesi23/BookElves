0583e9110259277ca59493e196bdb49d17c76ed6
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-04-30 21:53:12 +0300

    service -> add post

---
+++ BookElves/src/service/post.ts
@@ -0,0 +1,121 @@
+import { ref, push, get, query, orderByChild, update, equalTo } from 'firebase/database';
+import { db } from '../config/firebase-config';
+import { getAllUsers } from './user';
+
+
+export const addPost = async (author: string, title: string, description: string, imagePost: string, image: string, category: string) => {
+    return push(ref(db, 'posts'), {
+        author,
+        title,
+        description,
+        imagePost,
+        likes: 0,
+        createdOn: Date.now(),
+        image,
+        category,
+    });
+};
+
+export const getAllPosts = async (search: string) => {
+    const snapshot = await get(query(ref(db, 'posts'), orderByChild('createdOn')));
+    if (!snapshot.exists()) {
+        return [];
+    }
+
+    const posts = Object.keys(snapshot.val()).map(key => ({
+        id: key,
+        ...snapshot.val()[key],
+        createdOn: new Date(snapshot.val()[key].createdOn).toString(),
+        likedBy: snapshot.val()[key].likedBy ? Object.keys(snapshot.val()[key].likedBy) : [],
+        imageUrl: snapshot.val().imageUrl,
+    }))
+        .filter(p => p.title?.toLowerCase().includes(search.toLowerCase()));
+
+    return posts;
+};
+
+export const getAllPostsByUser = async (username: string) => {
+    const snapshot = await get(query(ref(db, 'posts'), orderByChild('createdOn')));
+    if (!snapshot.exists()) {
+        return [];
+    }
+
+    const posts = Object.keys(snapshot.val()).map(key => ({
+        id: key,
+        ...snapshot.val()[key],
+        createdOn: new Date(snapshot.val()[key].createdOn).toString(),
+        likedBy: snapshot.val()[key].likedBy ? Object.keys(snapshot.val()[key].likedBy) : [],
+        imageUrl: snapshot.val().imageUrl,
+    }))
+
+    return posts.filter(p => p.author === username);
+};
+
+export const getPostById = async (id: string) => {
+
+    const snapshot = await get(ref(db, `posts/${id}`));
+    if (!snapshot.exists()) {
+        return null;
+    }
+
+    const post = [{
+        id,
+        ...snapshot.val(),
+        createdOn: new Date(snapshot.val().createdOn).toString(),
+        likedBy: snapshot.val().likedBy ? Object.keys(snapshot.val().likedBy) : [],
+        imageUrl: snapshot.val().imageUrl,
+    }];
+
+    return post;
+};
+
+
+export const addLike = (username: string, postId: string, likesCount: number) => {
+    const updateLikes: { [key: string]: any } = {};
+    updateLikes[`/posts/${postId}/likedBy/${username}`] = true;
+    updateLikes[`/posts/${postId}/likes/`] = likesCount;
+    updateLikes[`/users/${username}/likedPosts/${postId}`] = true;
+    return update(ref(db), updateLikes);
+};
+
+
+export const removeLike = (username: string, postId: string, likes: number) => {
+    const updateLikes: { [key: string]: any } = {};
+    updateLikes[`/posts/${postId}/likedBy/${username}`] = null;
+    updateLikes[`/posts/${postId}/likes/`] = likes;
+    updateLikes[`/users/${username}/likedPosts/${postId}`] = null;
+
+    return update(ref(db), updateLikes);
+};
+
+export const deletePost = async (postId: string) => {
+    const user = await getAllUsers();
+    user.forEach(u => {
+        if (u.likedPosts) {
+            if (u.likedPosts.includes(postId)) {
+                const updateLikes: { [key: string]: any } = {};
+                updateLikes[`/users/${u.username}/likedPosts/${postId}`] = null;
+                update(ref(db), updateLikes);
+            }
+        }
+
+    })
+    const deletePost: { [key: string]: any } = {};
+    deletePost[`posts/${postId}`] = null;
+    update(ref(db), deletePost)
+}
+
+
+export const getPostsByCategory = async (category: string) => {
+    const snapshot = await get(query(ref(db, 'posts'), orderByChild('category'), equalTo(category)));
+    if (!snapshot.exists()) {
+        return [];
+    }
+    return Object.keys(snapshot.val()).map(key => ({
+        id: key,
+        ...snapshot.val()[key],
+        createdOn: new Date(snapshot.val()[key].createdOn).toString(),
+        likedBy: snapshot.val()[key].likedBy ? Object.keys(snapshot.val()[key].likedBy) : [],
+        imageUrl: snapshot.val().imageUrl,
+    }))
+}
