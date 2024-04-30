82a379d305129795cbfef8e3c5ede1b53942c10c
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-04-30 21:52:46 +0300

    add bootstrap in index.tsx

--- BookElves/src/index.tsx
+++ BookElves/src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from 'solid-js/web';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css'
 import App from './App'
