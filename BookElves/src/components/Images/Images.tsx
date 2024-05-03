af5a4a3f78310723b9ae3d4d065ebb2466f42df4
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-05-03 17:07:11 +0300

    add images component

--- BookElves/src/components/Images/Images.tsx
+++ BookElves/src/components/Images/Images.tsx
@@ -1,40 +1,24 @@
-// import { get } from "firebase/database";
-// import { useAppContext } from "../../context/appContext";
-// import { useState } from "react";
-// import { ref } from "firebase/storage";
+import { useEffect, useState } from "react";
+import { useAppContext } from "../../context/appContext";
+import { getUserByUsername } from "../../service/user";
 
-// const Image=()=>{
-// const [data, setData] = useState<any>(null);
-// const { userData } = useAppContext();
-// const [image, setImage] = useState<string>('');
-// const [error, setError] = useState(false);
-// const [errorMessage] = useState('');
-// const [loading, setLoading] = useState(false);
+const Image = ({ author }: string | any) => {
+    const [user, setUser] = useState<any>(null);
+    const { userData } = useAppContext();
 
-// const handleImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const file = e.target.files && e.target.files[0];
+    useEffect(() => {
+        (async () => {
+            const result = await getUserByUsername(author);
+            setUser(result.val());
+        })()
 
-//     if (file) {
-//         const imgURL = await saveImage(file);
-//         imgURL && setImage(imgURL);
-//         update(ref(db, `users/${userData.username}/`), { image: imgURL })
-//     }
-// };
+    }, [userData]);
+    return (
+        <>
+            {user?.image ? <img className="img" src={user?.image} alt="user" /> : <span className="letter">{user?.handle[0]}</span>}
+        </>
+    )
+}
+export default Image;
 
-// const handleSubmit = async () => {
-//     update(ref(db, `users/${userData.username}/`), { image: image });
-// };
 
-// const loadProfile = () => {
-//     setImage(userData.image);
-// };
-
-// return (
-//     userData && <div>
-//         <h1>Profile</h1>
-//         {userData.image && <img src={userData?.image} alt="profile" />}
-//         {userData}
-//     </div>
-// )
-
-// }
