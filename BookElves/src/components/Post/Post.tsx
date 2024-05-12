1a9b3bc1b442fc0233bb1d33579d9ec0b724b7ff
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-05-12 17:45:48 +0300

    add like functionality to Post component

--- BookElves/src/components/Post/Post.tsx
+++ BookElves/src/components/Post/Post.tsx
@@ -4,6 +4,7 @@ import { removeLike, addLike } from "../../service/post";
 import Image from "../Images/Images";
 import { NavLink } from "react-router-dom";
 import './Post.css';
+import { useState } from "react";
 
 type Post = {
     id: string;
@@ -29,15 +30,38 @@ type PostProps = {
 
 const Post: React.FC<PostProps> = ({ post, setPosts }) => {
     const { userData } = useAppContext();
+    const [isLiked, setIsLiked] = useState<boolean>(false);
 
+    const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+       if(userData){
+        if(event.target.checked){
+            addLike(userData.username, post.id, post.likes + 1);
+       }else{
+        removeLike(userData.username, post.id, post.likes - 1);
+       }
+       }
+    }
     const handleLike = async () => {
+        // if (userData) {
+        //     if (post.likedBy.includes(userData?.username)) {
+        //         await removeLike(userData.username, post.id, post.likes - 1);
+
+        //     } else {
+        //         await addLike(userData.username, post.id, post.likes + 1);
+
+        //     }
         if (userData) {
+            let newLikeStatus;
             if (post.likedBy.includes(userData?.username)) {
-                await removeLike(userData.username, post.id, post.likes - 1);
-
+                // Ensure post.likes is a number before decrementing
+                let likes = isNaN(post.likes) ? 0 : post.likes;
+                await removeLike(userData.username, post.id, likes - 1);
+                newLikeStatus = false;
             } else {
-                await addLike(userData.username, post.id, post.likes + 1);
-
+                // Ensure post.likes is a number before incrementing
+                let likes = isNaN(post.likes) ? 0 : post.likes;
+                await addLike(userData.username, post.id, likes + 1);
+                newLikeStatus = true;
             }
 
             setPosts((currentPosts: any) => currentPosts.map((p: any) => {
@@ -75,7 +99,13 @@ const Post: React.FC<PostProps> = ({ post, setPosts }) => {
 
                 </div>
                 <div className="heart-container" title={post.likedBy.includes(userData?.username) ? 'Unlike' : 'Like'} onClick={handleLike}>
-                    <input type="checkbox" className="checkbox" id="Give-It-An-Id" checked={post.likedBy.includes(userData?.username)} />
+                    {/* <input type="checkbox" className="checkbox" id="Give-It-An-Id" checked={post.likedBy.includes(userData?.username)} /> */}
+                    <input
+                        type="checkbox"
+                        className="checkbox"
+                        checked={post.likedBy.includes(userData?.username)}
+                        onChange={handleCheckboxChange}
+                    />
                     <div className="svg-container">
                         <svg viewBox="0 0 24 24" className="svg-outline" xmlns="http://www.w3.org/2000/svg">
                             <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z"></path>
