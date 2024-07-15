2d3d90f561485369bc9fb8713206ec17122dece0
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-07-15 16:53:31 +0300

    add Friends view

---
+++ BookElves/src/views/Friends/Friends.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import FriendsList from "../../components/FriendsList/FriendsList";
+import FriendsRequest from "../../components/FriendsRequest/FriendsRequest";
+import FriendsBlock from "../../components/FriendsBlock/FriendsBlock";
+import Search from "../../components/Search/Search";
+
+const Friends = () => {
+    const [view, setView] = useState('friendsList');//status->default
+
+    const handleView = (view: string) => {
+        switch (view) {
+            case 'friendsList':
+                return <FriendsList />
+            case 'friendsRequest':
+                return <FriendsRequest />
+            case 'block':
+                return <FriendsBlock/>
+            default:
+                return <FriendsList/>
+        }
+    };
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => setView('friendsList')}>Friends</button>
+                <button onClick={() => setView('friendsRequest')}>Friend Requests</button>
+                <button onClick={() => setView('block')}>Block</button>
+            <Search/>
+            </div>
+            {handleView(view)}
+        </div>
+    );
+
+}
+export default Friends;
