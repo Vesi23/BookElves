0af1762057e5f3f9eb870e233a3e3b4daccb8cb6
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-09-08 01:30:18 +0300

    Add category images to Home view

--- BookElves/src/views/Home/Home.tsx
+++ BookElves/src/views/Home/Home.tsx
@@ -1,19 +1,44 @@
 import { NavLink } from "react-router-dom";
+import fantasyImage from '../../img/fantasy.jpeg';
+import scifiImage from '../../img/scifi.jpeg';
+import romanceImage from '../../img/romance.jpeg';
+import autobiographyImage from '../../img/autobiography.jpeg';
+import historicalImage from '../../img/historical.jpeg';
+import biographyImage from '../../img/biography.jpeg';
+import nonFictionImage from '../../img/nonFiction.jpeg';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="home-view">
-      <NavLink to='/category/fantasy' className='nav-link'>Fantasy</NavLink>
-      <NavLink to='/category/sci-fi' className='nav-link'>Sci-Fi</NavLink>
+       <NavLink to='/category/fantasy' className='nav-link'>
+                <img src={fantasyImage} alt='Fantasy' className='category-image' />
+                Fantasy
+            </NavLink>
+      {/* <NavLink to='/category/fantasy' className='nav-link'>Fantasy</NavLink> */}
+      <NavLink to='/category/sci-fi' className='nav-link'>
+      <img src={scifiImage} alt='Sci-Fi' className='category-image' />
+      Sci-Fi
+      </NavLink>
       <NavLink to='/category/mystery' className='nav-link'>Mystery</NavLink>
-      <NavLink to='/category/romance' className='nav-link'>Romance</NavLink>
+      <NavLink to='/category/romance' className='nav-link'>
+      <img src={romanceImage} alt='Romance' className='category-image' />
+      Romance
+      </NavLink>
       <NavLink to='/category/horror' className='nav-link'>Horror</NavLink>
       <NavLink to='/category/thriller' className='nav-link'>Thriller</NavLink>
-      <NavLink to='/category/historical-fiction' className='nav-link'>Historical Fiction</NavLink>
-      <NavLink to='/category/non-fiction' className='nav-link'>Non-Fiction</NavLink>
-      <NavLink to='/category/biography' className='nav-link'>Biography</NavLink>
-      <NavLink to='/category/autobiography' className='nav-link'>Autobiography</NavLink>
+      <NavLink to='/category/historical-fiction' className='nav-link'>
+      <img src={historicalImage} alt='Historical Fiction' className='category-image' />
+      Historical Fiction</NavLink>
+      <NavLink to='/category/non-fiction' className='nav-link'>
+      <img src={nonFictionImage} alt='Non-Fiction' className='category-image' />
+      Non-Fiction</NavLink>
+      <NavLink to='/category/biography' className='nav-link'>
+      <img src={biographyImage} alt='Biography' className='category-image' />
+      Biography</NavLink>
+      <NavLink to='/category/autobiography' className='nav-link'>
+      <img src={autobiographyImage} alt='Autobiography' className='category-image' />
+      Autobiography</NavLink>
     </div>
   );
 }
